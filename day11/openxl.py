01f699c3d61e5f39464018bc3932613410654f3f
Gautam G <gautam26g@gmail.com>
2026-03-24 21:27:44 +0530

    final edits

--- day11/openxl.py
+++ day11/openxl.py
@@ -1,9 +1,20 @@
-from openpyxl import load_workbook
+import openpyxl
+from openpyxl import load_workbook, Workbook
 wb=load_workbook(r"C:\Users\Gautam G\OneDrive\Desktop\students.xlsx")
 sheet=wb.active
+highest=-1
+name1=""
+new_wb=Workbook()
+new_sheet=new_wb.active
+new_sheet.append(["Name", "Marks"])
 for row in sheet.iter_rows(min_row=2,values_only=True):
     name,marks=row
-    highest=0
+    
     if marks>highest:
-        highest=marks   
-print(f"The highest marks are {highest} obtained by {name}.")
+        highest=marks
+        name1=name 
+    if marks>=75:
+        new_sheet.append([name, marks])  
+print(f"The highest marks are {highest} obtained by {name1}.")
+new_wb.save(r"C:\Users\Gautam G\OneDrive\Desktop\top_students.xlsx")
+print("New Excel file with top students created successfully.")
